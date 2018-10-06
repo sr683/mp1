860f87ff56f9749ee8300af95e854c8944395e3d
shafiur <sr683@njit.edu>
2018-10-06 18:40:40 -0400

    Fix: start getting the file name

--- public/index.php
+++ public/index.php
@@ -6,30 +6,36 @@
  * Time: 5:26 PM
  */
 
-main::start();
+main::start( "studentinfo.csv");
 
 class main {
 
-    static public function start() {
-
-        $file = fopen( "studentinfo.csv",  "r");
-
-        while (! feof($file))
-        {
-
-            $record [] = fgetcsv($file);
-
-            $record [] = $record;
-        }
-
-        fclose($file);
-        print_r($record);
-
+    static public function start($filename) {
+        
+        $records = csv::getRecords($filename); 
+        print_r($records);           
 
     }
 
-
-
 }
 
+class csv {
+    
+    
+    static public function getRecords($filename) {
+        
+        $file = fopen( $filename, "r");
 
+        while (! feof($file)) 
+        {
+
+            $record  = fgetcsv($file);
+
+            $records [] = $record;
+        }
+
+        fclose($file);
+        return$records;
+    }
+    
+}
