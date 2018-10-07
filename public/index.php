999a22a28116dfe2729607d89d947ca4dc1eb5ca
shafiur <sr683@njit.edu>
2018-10-06 22:12:20 -0400

    Fix: loodping through each records

--- public/index.php
+++ public/index.php
@@ -20,18 +20,32 @@ class main {
 
 }
 
+class html {
+
+    public static function generateTable ($records) {
+
+        $count = 0;
+        foreach (records as $record);
+            if($count == 0 ) {
+                $array = $record-> returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r ($values);
+
+            } else {
+
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+
+            }
+            $count++;
 
 
+    }
 
-
-
-
-
-
-
-
-
-
+}
 
 
 class csv {
